32ce9443e630347b8defef162b5bf1c57bcca2a1
YuTin <yutin1987@users.noreply.github.com>
2017-05-21 01:50:49 +0800

    feat(demo): added clipboard

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+
+import 'primer-css/build/build.css';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Clippy from 'react-icons/lib/go/clippy';
+import GitHub from 'react-icons/lib/go/mark-github';
+import Clipboard from 'clipboard';
 import InApp from './inapp';
 import './index.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    new Clipboard('.copy'); // eslint-disable-line
+  }
+
   render() {
     const useragent = navigator.userAgent || navigator.vendor || window.opera;
     const inapp = new InApp(useragent);
+    let value = `${useragent}\n`;
+    if (navigator) for (let key in navigator) value += `\n${key}: ${navigator[key]}`; // eslint-disable-line
+
     return (
       <div>
-        <div>UserAgent: {useragent}</div>
+        <div><input id="useragent" value={value} style={{ width: '100%' }} /></div>
         <div>
-          <a className="github-button" href={`https://github.com/f2etw/detect-inapp/issues/new?title=%5BUserAgent%5D&body=${encodeURIComponent(navigator.userAgent)}`} aria-label="Issue f2etw/detect-inapp on GitHub">Share UserAgent</a>
+          <span className="input-group-button">
+            <button className="btn copy" data-clipboard-target="#useragent">
+              <Clippy />&nbsp;Copy UserAgent
+            </button>
+            <a className="btn" target="inapp" href={`https://github.com/f2etw/detect-inapp/issues/new?title=%5BUserAgent%5D&body=${encodeURIComponent(value)}`}>
+              <GitHub />&nbsp;Share
+            </a>
+          </span>
         </div>
         <hr />
         <div>os: {inapp.os}</div>
