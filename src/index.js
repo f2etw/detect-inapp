5d0d9477944b7d8b831971d177912c92b19fbe87
YuTin <yutin1987@users.noreply.github.com>
2017-05-20 23:44:53 +0800

    feat(demo): added github link into demo page

--- src/index.js
+++ src/index.js
@@ -14,6 +14,12 @@ class App extends Component {
         <div>os: {inapp.os}</div>
         <div>browser: {inapp.browser}</div>
         <div>isMobile(): {inapp.isMobile() ? 'true' : 'false'}</div>
+        <hr />
+        <div>
+          <a className="github-button" href="https://github.com/f2etw/detect-inapp/issues" data-size="large" data-icon="octicon-issue-opened" aria-label="Issue f2etw/detect-inapp on GitHub">Issue</a>&nbsp;
+          <a className="github-button" href="https://github.com/f2etw/detect-inapp/fork" data-size="large" data-icon="octicon-repo-forked" aria-label="Fork f2etw/detect-inapp on GitHub">Fork</a>&nbsp;
+          <a className="github-button" href="https://github.com/f2etw/detect-inapp" data-size="large" data-icon="octicon-star" aria-label="Star f2etw/detect-inapp on GitHub">Star</a>
+        </div>
       </div>
     );
   }
