44089d1b8cb4f4cd5252a3e042431362ca811654
patw <patw.hi@gmail.com>
2017-05-22 00:03:00 +0800

    Added Windows's user-agent strings in test case

--- src/__test__/inapp.test.js
+++ src/__test__/inapp.test.js
@@ -8,6 +8,15 @@ const DESKTOP = {
     ],
   },
   WINDOWS: {
+    CHROME: [
+      'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    ],
+    FIREFOX: [
+      'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0',
+    ],
+    IE11: [
+      'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    ],
   },
   UBUNTU: {
     CHROME: [
